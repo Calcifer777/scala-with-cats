4ba0aa0571f431e27661d60daf7801bfcb580461
calcifer777 <m.filip@hotmail.it>
2020-10-05 23:16:48 +0200

    chapter04 part1

---
+++ src/main/scala/chapter04/Monads.scala
@@ -0,0 +1,62 @@
+package chapter04
+
+import scala.language.higherKinds
+
+trait Monad[F[_]] {
+  def pure[A](value: A): F[A]
+  def flatmap[A, B](value: F[A])(func: A => F[B]): F[B]
+  def map[A, B](value: F[A])(func: A => B): F[B] = 
+    flatmap(value)(func andThen pure)
+}
+
+object Monad {
+
+  def pure[A, F[_]](value: A)(implicit m: Monad[F]): F[A] = 
+    m.pure(value)
+
+  def flatmap[A, F[_], B](value: F[A])(func: A => F[B])(implicit m: Monad[F]): F[B] = 
+    m.flatmap(value)(func)
+
+  def leftIdentity[A, F[_], B](a: A, func: A => F[B])(implicit m: Monad[F]): Boolean = 
+    flatmap(pure(a)(m))(func) == func(a)
+
+  def rightIdentity[A, F[_]](a: F[A])(implicit m: Monad[F]): Boolean = 
+    flatmap(a)(m.pure) == a
+
+  def associativity[A, B, C, F[_]](a: F[A], f: A => F[B], g: B => F[C])(implicit m: Monad[F]): Boolean = 
+    flatmap(flatmap(a)(f))(g) == flatmap(a)((va: A) => flatmap(f(va))(g))
+
+  type Id[A] = A
+
+  implicit val idMonad: Monad[Id] = new Monad[Id] {
+    def flatmap[A, B](value: Id[A])(func: A => Id[B]): Id[B] = func(value)
+    def pure[A](value: A): Id[A] = value
+  }
+
+}
+
+object Examples {
+
+  import cats.Monad
+  import cats.Eval
+
+  def foldRightUnsafe[A, B](l: List[A], acc: B)(f: (A, B) => B): B =
+    l match {
+      case head :: tail => f(head, foldRightUnsafe(tail, acc)(f))
+      case Nil => acc
+    }
+
+  def foldRightEval[A, B](l: List[A], acc: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
+    l match {
+      case head :: tail => Eval.defer(f(head, foldRightUnsafe(tail, acc)(f)))
+      case Nil => acc
+    }
+
+  /** Almost safe */
+  def foldRightSafe[A, B](l: List[A], acc: B)(f: (A, B) => B): B = 
+    foldRightEval(l, Eval.now(acc)){
+      (a: A, evB: Eval[B]) => 
+        evB.map((b: B) => f(a, b))
+    }.value
+
+}
---
+++ src/main/scala/chapter04/WriterMain.scala
@@ -0,0 +1,61 @@
+package chapter04
+
+import cats.data.Writer
+import cats.instances.vector._
+import cats.syntax.applicative._
+import cats.syntax.writer._
+
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+object WriterMain extends App {
+
+  def slowly[A](body: => A) =
+    try body finally Thread.sleep(100)
+
+  def factorial(n: Int): Int = {
+    val ans = slowly(if (n == 0) 1 else n * factorial(n-1))
+    println(f"fact ${n} ${ans}")
+    ans
+  }
+  
+  println("*** Logs from multithread ***")
+  Await.result(
+    Future.sequence(
+      Vector(
+        Future(factorial(4)),
+        Future(factorial(4))
+      )
+    ),
+    5.seconds
+  )
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  def factorialW(n: Int): Logged[Int] = 
+    for {
+      ans <- if (n == 0) 1.pure[Logged]
+             else { 
+               slowly { 
+                 factorialW(n-1).map(_ * n) 
+               } 
+             }
+      _ <- Vector(f"fact ${n} ${ans}").tell
+    } yield ans
+    
+  println("*** Logs from multithread with Writer context***")
+  val v = Await.result(
+    Future.sequence(
+      Vector(
+        Future(factorialW(4)),
+        Future(factorialW(4))
+      )
+    ),
+    5.seconds
+  )
+  v map (_.written) foreach (println)
+
+
+}
